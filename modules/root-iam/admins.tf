16a0ea08c93d8de18db7e91646119b933dc5ec33
leb4r <88212774+leb4r@users.noreply.github.com>
2022-04-19 19:17:13 -0500

    modules: update root-iam (#10)

--- modules/root-iam/admins.tf
+++ modules/root-iam/admins.tf
@@ -1,23 +1,31 @@
+locals {
+  admin_users = {
+    for admin in var.admin_users : admin.user_name => admin
+  }
+}
+
 ## admin role
 
 module "admin_role" {
   source  = "cloudposse/iam-role/aws"
-  version = "0.10.0"
+  version = "0.16.1"
 
-  name = "${module.this.id}-admin"
+  name = format("%s-admin", module.this.id)
 
   role_description = "Administrator role"
 
-  policy_document_count = 0
-  policy_description    = "Administrator access"
+  policy_document_count = 0 # do not attach a policy to role
+
   principals = {
-    AWS = ["arn:aws:iam::${data.aws_caller_identity.this.account_id}:root"]
+    AWS = [
+      format("arn:%s:iam::%s:root", local.partition, local.account_id)
+    ]
   }
 }
 
 resource "aws_iam_role_policy_attachment" "admin" {
   role       = module.admin_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  policy_arn = format("arn:%s:iam::aws:policy/AdministratorAccess", local.partition)
 }
 
 ## admin group
@@ -36,13 +44,13 @@ data "aws_iam_policy_document" "assume_admin" {
 }
 
 resource "aws_iam_policy" "assume_admin" {
-  name        = "${module.this.id}-assume-admin"
+  name        = format("%s-assume-admin", module.this.id)
   description = "Allow assumption of ${module.admin_role.name}"
   policy      = data.aws_iam_policy_document.assume_admin.json
 }
 
 resource "aws_iam_group" "admins" {
-  name = "${module.this.id}-admins"
+  name = format("%s-admins", module.this.id)
 }
 
 resource "aws_iam_group_policy_attachment" "assume_admin" {
@@ -56,7 +64,7 @@ module "admin_user" {
   source  = "cloudposse/iam-user/aws"
   version = "0.8.1"
 
-  for_each = { for admin in var.admin_users : admin.user_name => admin }
+  for_each = local.admin_users
 
   user_name             = each.value.user_name
   pgp_key               = lookup(each.value, "pgp_key", null)
